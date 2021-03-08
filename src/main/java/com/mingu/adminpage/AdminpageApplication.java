1d7bc8dadccf9fd17d136eb4adb32c92d0b8754d
mingu <qnbz209@gmail.com>
2021-03-08 13:36:46 +0900

    first commit

---
+++ src/main/java/com/mingu/adminpage/AdminpageApplication.java
@@ -0,0 +1,13 @@
+package com.mingu.adminpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminpageApplication.class, args);
+	}
+
+}

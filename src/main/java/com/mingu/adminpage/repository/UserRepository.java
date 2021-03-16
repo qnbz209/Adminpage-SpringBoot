ce6243c135fdd57569a529efe418ae7f91c2c7b9
이민구 <mingu@MacBookPro.local>
2021-03-16 21:55:25 +0900

    create Entity and Repository

---
+++ src/main/java/com/mingu/adminpage/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mingu.adminpage.repository;
+
+import com.mingu.adminpage.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
